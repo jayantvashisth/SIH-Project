4d4eba1f70942025c81cb002ba3ed64cef3248e0
jayant vashisth <vashisthjayant@gmail.com>
2022-03-21 14:19:38 +0530

    password hashed

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,6 +3,10 @@ const app = express();
 const router = express.Router();
 const User = require("../modules/user");
 
+//Hashing password
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 
 router.post('/signup',async(req,res)=>{
@@ -11,11 +15,15 @@ router.post('/signup',async(req,res)=>{
         res.status(400).send("user already exists")
     }
     else{
+
+        const salt =await bcrypt.genSalt(saltRounds);
+        const hashed_pass =await bcrypt.hash(req.body.password, salt);
+
         res.send(req.body);
         const user = new User({
             username : req.body.username,
             email : req.body.email,
-            password : req.body.password
+            password : hashed_pass
         });
         user.save();
     }
